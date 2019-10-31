b5fd52e411b08c32e60e87d36230b1d61d10ea5d
sigit santoso <rjugadi@yahoo.com>
2019-10-31 22:51:39 +0700

    commit

---
+++ app/Items.php
@@ -0,0 +1,14 @@
+<?php 
+
+/**
+ * 
+ */
+class Items
+{
+	public $item1='Kaos';
+
+	public $item2='Baju';
+
+	public $item3='Tas';
+
+}
--- index.php
+++ index.php
@@ -1,7 +1,24 @@
 <?php 
-
+/*
 require_once './app/ClassSatu.php';
 
 $Obcj = new ClassSatu; //Create Object onstance from class
 
 $Obcj->b(); //Call method from Class
+
+*/
+
+require_once './app/Items.php';
+
+$ObjItems1 = new Items();
+
+$newItem= $ObjItems1->item1='kaos oblong';
+
+var_dump('Obect instance  new items value [ '.$newItem.'] overwrite default value with new value </br>');
+
+$ObjItems2 = new Items();
+
+var_dump('Object new instance call same items propertis [ '.$ObjItems2->item1.' ] defult value');
+
+
+
